60d880ba777b2e52ec995ea85ba07e896f64188e
jeffairsonn <thomasbelenuspro@gmail.com>
2025-04-24 18:10:18 +0200

    change metada

--- src/pages/index.js
+++ src/pages/index.js
@@ -40,8 +40,8 @@ export default function Home() {
   const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
+      title={`${siteConfig.title}`}
+      description="Documentation de Couvrup - la plateforme numéro pour les prestations de toitures"
     >
       <HomepageHeader />
     </Layout>
